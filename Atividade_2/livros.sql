8f75a448044221946f75175a83f8025d5f36461d
Hugo Loiola <hugoloiola.1@gmail.com>
2024-09-28 17:08:09 -0300

    livros.sql Feito

--- Atividade_2/livros.sql
+++ Atividade_2/livros.sql
@@ -1,50 +1,65 @@
-CREATE TABLE livros (
-    id_livro INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(50),
-    autor VARCHAR(50),
-    ano_publicacao YEAR,
-    preco DECIMAL(10,2),
+CREATE TABLE Livros
+(
+    id_livro           INT AUTO_INCREMENT PRIMARY KEY,
+    titulo             VARCHAR(50),
+    autor              VARCHAR(50),
+    ano_publicacao     YEAR,
+    preco              DECIMAL(10, 2),
     quantidade_estoque INT
 );
 -- Livro 1
-insert into livros (titulo, autor, ano_publicacao, preco, quantidade_estoque)
+insert into Livros (titulo, autor, ano_publicacao, preco, quantidade_estoque)
 values ('Introdução ao SQL', 'João Silva', 2015, 50.00, 10);
 
 -- Livro 2
-insert into  livros (titulo, autor, ano_publicacao, preco, quantidade_estoque)
+insert into Livros (titulo, autor, ano_publicacao, preco, quantidade_estoque)
 values ('Mastering SQL', 'Maria Souza', 2018, 120.00, 5);
 
 -- Livro 3
-insert into livros (titulo, autor, ano_publicacao, preco, quantidade_estoque)
+insert into Livros (titulo, autor, ano_publicacao, preco, quantidade_estoque)
 values ('SQL para Iniciantes', 'Paulo Santos', 2020, 75.00, 8);
 
 -- Listar todos os livros, mostrando o título, autor e preço.
-SELECT titulo, autor, preco FROM livros;
+SELECT titulo, autor, preco
+FROM Livros;
 
 -- Selecionar todos os livros publicados a partir de 2018, mostrando título e ano de publicação.
-SELECT titulo, ano_publicacao FROM livros where ano_publicacao >= 2018;
+SELECT titulo, ano_publicacao
+FROM Livros
+where ano_publicacao >= 2018;
 
 -- Exibir os livros cujo preço esteja entre 60.00 e 100.00, mostrando o título e o preço.
-SELECT titulo, preco FROM livros WHERE preco <= 100.00 AND preco >= 60.00;
+SELECT titulo, preco
+FROM Livros
+WHERE preco <= 100.00
+  AND preco >= 60.00;
 
 -- Listar os livros com quantidade em estoque menor que 10, mostrando o título e a quantidade.
-SELECT titulo, quantidade_estoque FROM livros where quantidade_estoque < 10;
+SELECT titulo, quantidade_estoque
+FROM Livros
+where quantidade_estoque < 10;
 
 -- O preço do livro "SQL para Iniciantes" aumentou para 80.00. Faça essa atualização.
-UPDATE livros SET preco = 80.00 WHERE titulo = 'SQL para Iniciantes';
+UPDATE Livros
+SET preco = 80.00
+WHERE titulo = 'SQL para Iniciantes';
 
 -- O estoque do livro "Introdução ao SQL" foi atualizado para 15. Atualize essa informação.
-UPDATE livros SET quantidade_estoque = 15 WHERE titulo = 'Introdução ao SQL';
+UPDATE Livros
+SET quantidade_estoque = 15
+WHERE titulo = 'Introdução ao SQL';
 
 -- Um livro foi removido da livraria. Exclua o livro "Mastering SQL" da tabela.
-DELETE FROM livros WHERE titulo = 'Mastering SQL';
+DELETE
+FROM Livros
+WHERE titulo = 'Mastering SQL';
 
 -- Calcule o preço total dos livros disponíveis em estoque
 SELECT SUM(preco * quantidade_estoque) AS preco_total_estoque
-FROM livros;
+FROM Livros;
 
 -- Encontre o livro mais caro da tabela.
 SELECT titulo, autor, preco
-FROM livros
+FROM Livros
 ORDER BY preco DESC
 LIMIT 1;
